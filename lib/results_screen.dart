8496ab73b95270d77f487548d2854814b351f6a7
Mohamed Tarek <m.tarek@moveitgate.com>
2026-03-09 12:49:22 +0200

    89

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/data/questions.dart';
+import 'package:flutter_quiz/questions_summery.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswers});
@@ -13,7 +14,7 @@ class ResultsScreen extends StatelessWidget {
         'question_index': i,
         'question': questions[i].text,
         'correct_answer': questions[i].answers[1],
-        'cuser_answer': choosenAnswers[i],
+        'user_answer': choosenAnswers[i],
       });
     }
     return summery;
@@ -21,6 +22,11 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var summeryData = getSummery();
+    var numTotalQuestions = questions.length;
+    var numCorrectQuestions = summeryData.where((data){
+      return data['correct_answer'] == data['user_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -29,13 +35,13 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text("You answered X out of Y questions correctly"),
+            Text("You answered $numCorrectQuestions out of $numTotalQuestions questions correctly"),
             SizedBox(height: 10),
             Text("List of answers and questions..."),
             SizedBox(height: 10),
-            ...getSummery().forEach((key, value) {
-              Text(e);
-            }),
+            QuestionsSummery(getSummery()),
+            SizedBox(height: 10),
+            TextButton(onPressed: () {}, child: const Text("Restart Quiz!"))
           ],
         ),
       ),
